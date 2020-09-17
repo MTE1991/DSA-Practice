15857b754974c43d99e79e3ca83f1aff7560d937
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-09-17 08:42:46 +0000

    Update linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -5,12 +5,13 @@ def linearSearch(a, v):
     i = 0
     
     # if value is found in array, return index
-    # of the value:
+    # of the value, otherwise return 0:
     while i <= len(a):
         if a[i] == val:
             return i
             break
         i += 1
+        else: return 0
 
 arr = [5, 2, 4, 6, 1, 3]
 value = 6
