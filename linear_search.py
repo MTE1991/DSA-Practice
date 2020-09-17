102c3e23d6bf3dceccae9f3ccd2e3daf264c5571
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-09-17 08:38:04 +0000

    Create linear_search.py

---
+++ linear_search.py
@@ -0,0 +1,17 @@
+# Perform linear search for value "v"
+# in array "a".
+
+def linearSearch(a, v):
+    i = 0
+    
+    # if value is found in array, return index
+    # of the value:
+    while i <= len(a):
+        if a[i] == val:
+            return i
+            break
+        i += 1
+
+arr = [5, 2, 4, 6, 1, 3]
+value = 6
+print(linearSearch(arr, value))
