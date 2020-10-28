3a70ef4dc8eb9418d39e7224ad119e440b261bb2
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-10-28 23:12:05 +0000

    Update shellsort.cpp

--- shellsort.cpp
+++ shellsort.cpp
@@ -1,17 +1,15 @@
 #include  <iostream> 
 using namespace std; 
   
-int shellSort(int arr[], int n) 
+int shell_sort(int arr[], int n) 
 { 
     // Start with a big gap, then reduce the gap 
-    for (int gap = n/2; gap > 0; gap /= 2) 
-    { 
+    for (int gap = n/2; gap > 0; gap /= 2) { 
         /* Do a gapped insertion sort for this gap size. 
         The first gap elements a[0..gap-1] are already in gapped order 
         keep adding one more element until the entire array is 
         gap sorted */
-        for (int i = gap; i < n; i += 1) 
-        { 
+        for (int i = gap; i < n; i += 1) { 
             // add a[i] to the elements that have been gap sorted 
             // save a[i] in temp and make a hole at position i 
             int temp = arr[i]; 
