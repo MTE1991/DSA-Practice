065fe820bddd38713f1d1c8f6c51a29bc88e4d54
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-11-07 11:24:28 +0600

    Update quicksort2.cpp

--- quicksort2.cpp
+++ quicksort2.cpp
@@ -10,31 +10,31 @@ void swap(int *a, int *b)
 /* This function takes last element as pivot, places the pivot element at its correct
 position in sorted array, and places all smaller (smaller than pivot) to left of pivot and
 all greater elements to right of pivot */
-int partition(int arr[], int low, int high) { 
-    int pivot = arr[high];    // pivot 
-    int i = (low - 1);  // Index of smaller element   
+int partition(int arr[], int p, int r) { 
+    int q = arr[r];    // pivot 
+    int i = (p - 1);  // Index of smaller element   
 
-    for (int j = low; j <= high- 1; j++) {     
-        if (arr[j] <= pivot) { 
+    for (int j = p; j <= r - 1; j++) {     
+        if (arr[j] <= q) { 
             i++;    // increment index of smaller element 
             swap(&arr[i], &arr[j]); 
         } 
     } 
-    swap(&arr[i + 1], &arr[high]); 
+    swap(&arr[i + 1], &arr[r]); 
     return (i + 1); 
 } 
 
   
 /* QuickSort procedure (tail recursion)
 arr[] --> Array to be sorted, 
-low  --> Starting index, 
-high  --> Ending index */
-void quickSort(int arr[], int low, int high) { 
-    while(low < high) { 
-        /* pi is partitioning index, arr[p] is now at right place */
-        int pi = partition(arr, low, high); 
-        quickSort(arr, low, pi - 1); 
-        low = pi + 1;
+p  --> Starting index, 
+r  --> Ending index */
+void quick_sort(int arr[], int p, int r) { 
+    while (p < r) { 
+        /* q is partitioning index, arr[p] is now at right place */
+        int q = partition(arr, p, r); 
+        quick_sort(arr, p, q - 1); 
+        p = q + 1;
     } 
 } 
 
@@ -43,7 +43,7 @@ int main()
     int arr[] = {10, 7, 8, 19, 1, 50, 9}; 
     int n = sizeof(arr)/sizeof(arr[0]); 
 
-    quickSort(arr, 0, n-1); 
+    quick_sort(arr, 0, n-1); 
 
     printf("Sorted array: "); 
     for (int i=0; i < n; i++)
