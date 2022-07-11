b0e5c62f40ac7eb6ca127269dd4d22809e887d21
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2022-07-11 13:15:30 +0600

    Update algo1.py

--- algo1.py
+++ algo1.py
@@ -1,13 +1,3 @@
-def insertionSort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j = j - 1
-        arr[j + 1] = key
-    return arr
-
 def insertionSortReverse(arr):
     for i in range(1, len(arr)):
         key = arr[i]
@@ -18,14 +8,58 @@ def insertionSortReverse(arr):
         arr[j + 1] = key
     return arr
 
-a = [9, 6, 4, 12, 3, 5]
+def insertionSort(A):
+	for i in range(1, len(A)):
+		key = A[i]
+		j = i - 1
+		while j >= 0 and A[j] > key:
+			A[j + 1] = A[j]
+			j -= 1
+		A[j + 1] = key
 
-def bubbleSort(arr):
-    for i in range(len(arr)):
-        for j in range(0, len(arr) - i - 1):
-            if arr[i] > arr[i + 1]:
-                arr[i + 1], arr[i] = arr[i], arr[i + 1]
-    return arr
+#def selectionSort(A)
 
-print(insertionSort(a))
-print(bubbleSort(a))
+def bubbleSort(A):
+	for i in range(0, len(A)-1):
+		for j in range(0, len(A)-1):
+			if A[j + 1] <= A[j]:
+				A[j + 1], A[j] = A[j], A[j + 1]
+
+def mergeSort(A, left, right):
+	if left < right:
+		mid = (left + right) // 2
+		mergeSort(A, left, mid)
+		mergeSort(A, mid + 1, right)
+		merge(A, left, mid, right)
+
+def merge(A, p, q, r):
+	n1 = q - p + 1
+	n2 = r - q
+	L = [0] * n1
+	R = [0] * n2
+
+	for i in range(0, n1):
+		L[i] = A[p + i]
+	for i in range(0, n2):
+		R[i] = A[q + 1 + i]
+
+	i = j = 0
+	k = p 
+
+	while i < n1 and j < n2:
+		if L[i] <= R[j]:
+			A[k] = L[i]
+			i += 1
+		else:
+			A[k] = R[j]
+			j += 1
+		k += 1
+
+	while i < n1:
+		A[k] = L[i]
+		i += 1
+		k += 1
+	while j < n2:
+		A[k] = R[j]
+		j += 1
+		k += 1
