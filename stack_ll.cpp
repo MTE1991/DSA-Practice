41b7467789011cd9245ce6a4db80430c44c70756
MT Ekleel <mtahsinekleel02@gmail.com>
2023-09-06 13:28:49 +0600

    Update stack_ll.cpp

--- stack_ll.cpp
+++ stack_ll.cpp
@@ -3,85 +3,100 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
-	int data;
-	Node* next;	
+struct Node
+{
+    int data;
+    Node *next;
 };
 
-Node* top = nullptr; // top of stack
+Node *top = nullptr; // top of stack
 
-bool isEmpty() {
+bool isEmpty()
+{
     return top == nullptr;
 }
 
-bool isFull(Node* curr) {
-	return curr == nullptr;
+bool isFull(Node *curr)
+{
+    return curr == nullptr;
 }
 
-void push(int val) {
-    Node* newNode = new Node;
-    if (!isFull(newNode)) {
-    	newNode->data = val;
-	    newNode->next = top;
-	    top = newNode;
-    } else {
-    	cout << "Heap overflow!\n";
+void push(int val)
+{
+    Node *newNode = new Node;
+    if (!isFull(newNode))
+    {
+        newNode->data = val;
+        newNode->next = top;
+        top = newNode;
+    }
+    else
+    {
+        cout << "Heap overflow!\n";
     }
 }
 
-int pop() {
-    if (isEmpty()) {
+int pop()
+{
+    if (isEmpty())
+    {
         cout << "Stack is empty!\n";
         return -1;
     }
-    
+
     int poppedValue = top->data;
-    Node* temp = top;
+    Node *temp = top;
     top = temp->next;
-    delete temp; // free(temp) 
+    delete temp; // free(temp)
     return poppedValue;
 }
 
-int peek() {
-    if (isEmpty()) {
+int peek()
+{
+    if (isEmpty())
+    {
         cout << "Stack is empty!\n";
         return -1;
     }
     return top->data;
 }
 
-void display() {
-    if (isEmpty()) {
+void display()
+{
+    if (isEmpty())
+    {
         cout << "Stack is empty!\n";
         return;
     }
 
     cout << "Stack: ";
-    Node* curr = top;
-    while (curr) {
+    Node *curr = top;
+    while (curr)
+    {
         cout << curr->data << " ";
         curr = curr->next;
     }
     cout << endl;
 }
 
+int main()
+{
+    if (isEmpty())
+    {
+        cout << "Stack is empty!\n";
+    }
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    display();
+    cout << "Popping " << pop() << endl;
+    display();
+    cout << "Pushing 12\n";
+    push(12);
+    cout << "Peek() = " << peek() << endl;
+    display();
 
-int main() {
-	if (isEmpty()) {
-		cout << "Stack is empty!\n";
-	}
-	push(1);
-	push(2);
-	push(3);
-	push(4);
-	push(5);
-	display();
-	cout << "Popping " << pop() << endl;
-	display();
-	cout << "Pushing 12\n";
-	push(12);
-	cout << "Peek() = " << peek() << endl;
-	display();
-
-	return 0;
+    return 0;
 }
