035726c756092fb484376fdbe4ed6253406bd02b
MT Ekleel <mtahsinekleel02@gmail.com>
2024-03-22 14:13:07 +0600

    Add files via upload

---
+++ QuickSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[high], arr[i + 1]);
+    return i + 1;
+}
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
+    }
+}
+
+int main() {
+    vector<int> a = {5, 4, 12, 7, 9, 23};
+    int n = a.size();
+    quickSort(a, 0, n - 1);
+    for (auto i : a) {
+        cout << i << endl;
+    }
+    return 0;
+}
